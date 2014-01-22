6e1d6a847aebb13bd4225b183ec0f307af87b62f
Andrew Neitsch <andrew@neitsch.ca>
2014-01-23 03:31:23 +0700

    Add models and admin.

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from polls.models import Poll, Choice
 
-# Register your models here.
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.question
+
+class Choice(models.Model):
+    question = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.choice_text
